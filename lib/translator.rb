374dcd62d83e81d2afe619c848d1cd8e4c3cfcdc
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-18 04:37:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -36,6 +36,7 @@ def get_japanese_emoticon(file, emoticon)
     translator = load_library(file)
     
     translator.each_pair do |word, array|
+binding.pry
         if translator
         end
     end
